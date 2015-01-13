1036495077f815b092426588e94d282b160384b5
Rickard Gimerstedt <Rickard@ake1-desktop.(none)>
2015-01-13 22:39:43 +0100

    a little cleaner and easier to edit the prio list in cthun run in

--- MiscHelper/MiscHelper.lua
+++ MiscHelper/MiscHelper.lua
@@ -12,97 +12,88 @@ end
 -- automatic cthun run in order
 SLASH_CTHUNRUNIN1 = '/cthun'
 function SlashCmdList.CTHUNRUNIN()
-	local groupNumber = getSubGroupNumber()
-	local order = {} -- list of names in run in order
-	local playerClass = UnitClass("player")
-	local playerName = UnitName("player")
-
-	-- self explanatory
-	local function isHealer(class)
-		if class == "Druid" or class == "Priest" or class == "Paladin" then
-			return true
-		else
-			return false
-		end
-	end
-
-	-- self explanatory
-	local function isMelee(class)
-		if class == "Warrior" or class == "Rogue" then
-			return true
-		else
-			return false
-		end
-	end
-
-	-- add target
-	if UnitInParty("target") then
-		local name = UnitName("target")
-		table.insert(order, name)
-	else
-		sysout("You need to target the melee in your party.")
+	-- return if you're not targeting the melee
+	if UnitInParty("target") == nil then
+		sysout("You must target the melee in your group.")
 		return
 	end
 
-	-- add the healers
-	for i=1,GetNumPartyMembers() do
-		local class = UnitClass("party"..i)
-		local name = UnitName("party"..i)
-		if name ~= UnitName("target") then 
-			if isHealer(class) then
-				table.insert(order, name)
-			end
+	-- declaring some variables
+	local party = {
+		player = UnitClass("player"),
+		party1 = UnitClass("party1"),
+		party2 = UnitClass("party2"),
+		party3 = UnitClass("party3"),
+		party4 = UnitClass("party4")
+	}
+	local orderedWithGaps = {}
+	local ordered = {}
+	local orderedWithNames = {}
+
+	-- put target first, sort rest according to class
+	for k,v in pairs(party) do
+		if UnitName(k) == UnitName("target") then
+			table.insert(orderedWithGaps, 1, k)
+		elseif v == "Priest" then
+			table.insert(orderedWithGaps, 10, k)
+		elseif v == "Paladin" then
+			table.insert(orderedWithGaps, 10, k)
+		elseif v == "Shaman" then
+			table.insert(orderedWithGaps, 10, k)
+		elseif v == "Druid" then
+			table.insert(orderedWithGaps, 10, k)
+
+		elseif v == "Mage" then
+			table.insert(orderedWithGaps, 20, k)
+		elseif v == "Warlock" then
+			table.insert(orderedWithGaps, 20, k)
+
+		elseif v == "Hunter" then
+			table.insert(orderedWithGaps, 80, k)
+		elseif v == "Rogue" then
+			table.insert(orderedWithGaps, 90, k)
+		elseif v == "Warrior" then
+			table.insert(orderedWithGaps, 90, k)
 		end
 	end
 
-	-- add yourself if non-melee
-	if isMelee(playerClass) ~= true and UnitIsUnit("player", "target") ~= true then
-		table.insert(order, playerName)
-	end
-
-	-- add the non-melee/non-healers
-	for i=1,GetNumPartyMembers() do
-		local class = UnitClass("party"..i)
-		local name = UnitName("party"..i)
-		if name ~= UnitName("target") then
-			if isHealer(class) ~= true and isMelee(class) ~= true then
-				table.insert(order, name)
-			end
-		end
-	end
-
-	-- add yourself if you're melee
-	if isMelee(playerClass) and UnitIsUnit("player", "target") ~= true then
-		table.insert(order, playerName)
-	end
-
-	-- add the melee last
-	for i=1,GetNumPartyMembers() do
-		local class = UnitClass("party"..i)
-		local name = UnitName("party"..i)
-		if name ~= UnitName("target") then
-			if isHealer(class) == false and isMelee(class) == true then
-				table.insert(order, name)
-			end
+	-- put names into new table, ready for use
+	for i=1, table.getn(orderedWithGaps) do
+		if orderedWithGaps[i] ~= nil then
+			table.insert(ordered, orderedWithGaps[i])
 		end
 	end
 
 	-- fill empty spots
-	for i=table.getn(order), 5 do
-		table.insert(order, "Empty Spot")
+	for i=1, 5 do
+		if ordered[i] ~= nil then
+			local name = UnitName(ordered[i])
+			table.insert(orderedWithNames, name)
+		else
+			table.insert(orderedWithNames, "Empty Spot")
+		end
 	end
 
+	local function getSubGroupNumber()
+		if UnitInRaid("player") then
+			for i = 1 , GetNumRaidMembers() do
+				local name, _, subgroup = GetRaidRosterInfo(i)
+				if (name == UnitName("player")) then
+					return subgroup
+				end
+			end
+		end
+	end
+	local groupNumber = getSubGroupNumber()
+
 	-- send to chat
-	-- SendChatMessage("----- C'Thun run in order -----", "PARTY")
-
-	SendChatMessage("--------------- (1) "..order[1].." ---------------", "PARTY")
-
+	SendChatMessage("--------------- (1) "..UnitName(ordered[1]).." ---------------", "PARTY")
 	if mod(groupNumber, 2) == 0 then
-		SendChatMessage("(3) "..order[3].." ----- (2) "..order[2], "PARTY")
-		SendChatMessage("(5) "..order[5].." ----- (4) "..order[4], "PARTY")
+		SendChatMessage("(3) "..orderedWithNames[3].." ----- (2) "..orderedWithNames[2], "PARTY")
+		SendChatMessage("(5) "..orderedWithNames[5].." ----- (4) "..orderedWithNames[4], "PARTY")
 	else
-		SendChatMessage("(2) "..order[2].." ----- (3) "..order[3], "PARTY")
-		SendChatMessage("(4) "..order[4].." ----- (5) "..order[5], "PARTY")
+		SendChatMessage("(2) "..orderedWithNames[2].." ----- (3) "..orderedWithNames[3], "PARTY")
+		SendChatMessage("(4) "..orderedWithNames[4].." ----- (5) "..orderedWithNames[5], "PARTY")
 	end
 end
 
@@ -111,20 +102,10 @@ function sysout(msg, r, g, b)
 	g = g or 0.6
 	b = b or 1
 	if msg then		
-		DEFAULT_CHAT_FRAME:AddMessage("[PaladinHelper] " .. tostring(msg), r, g, b)		
+		DEFAULT_CHAT_FRAME:AddMessage("[MiscHelper] " .. tostring(msg), r, g, b)		
 	end
 end
 
-function getSubGroupNumber()
-	if UnitInRaid("player") then
-		for i = 1 , GetNumRaidMembers() do
-			local name, _, subgroup = GetRaidRosterInfo(i)
-			if (name == UnitName("player")) then
-				return subgroup
-			end
-		end
-	end
-end
 
 SLASH_RLUI1 = '/rl'
 function SlashCmdList.RLUI()	
