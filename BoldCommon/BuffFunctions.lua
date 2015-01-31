51b9362a451ec0fa1bcf9a52c4c193a2348ce5c8
boldilocks <kallerotmg@github.com>
2015-01-31 01:36:42 +0100

    fixed reporting debuffs on target.

--- BoldCommon/BuffFunctions.lua
+++ BoldCommon/BuffFunctions.lua
@@ -4,28 +4,35 @@ function ReportMissingDebuffsOnTarget()
 		BC.m("You are either in not in a raid or your target is not an enemy or you do not have a target.", BC_PREP)
 		return
 	end
-	local debuffsMissing = {}
-	for i=1, 16 do
-		db = UnitDebuff("target", i)
-		if db == nil then 
-			BC.r(UnitName("target").." has no debuffs!", BC_PREP)
-			break
-		end
-		if not string.find(db, "Spell_Shadow_CurseOfAchimonde") then table.insert(debuffsMissing, "Curse of Shadows") end
-		if not string.find(db, "Spell_Shadow_ChillTouch") then table.insert(debuffsMissing, "Curse of Elements") end
-		if not string.find(db, "Spell_Shadow_UnholyStrength") then table.insert(debuffsMissing, "Curse of Recklessness") end
-		if not string.find(db, "Spell_Nature_FaerieFire") then table.insert(debuffsMissing, "Faerie Fire") end
-		if not string.find(db, "Spell_Shadow_BlackPlague") then table.insert(debuffsMissing, "Shadow Weaving") end
-		if not string.find(db, "Ability_Warrior_Sunder") then table.insert(debuffsMissing, "Sunder Armor") end
-		if not string.find(db, "Spell_Holy_Holysmite") then table.insert(debuffsMissing, "SotC") end
+	if not UnitDebuff("target", 1) then
+		BC.r(UnitName("target").." has no debuffs!", BC_PREP)
+		return
 	end
-	if table.getn(debuffsMissing) > 0 then
-		local outputString = UnitName("target").." is missing"
-		for i = 1, table.getn(debuffsMissing) do
-			outputString = outputString.." "..debuffsMissing[i].."!"
-		end
-		BC.r(outputString)
+
+	local cos, coe, cor, ff, sw, sa = false, false, false, false, false, 0
+	local i = 1
+
+	while UnitDebuff("target", i) do
+		local texture, applications = UnitDebuff("target", i)
+		if string.find(texture, "Spell_Shadow_CurseOfAchimonde") then cos = true end
+		if string.find(texture, "Spell_Shadow_ChillTouch") then coe = true end
+		if string.find(texture, "Spell_Shadow_UnholyStrength") then cor = true end
+		if string.find(texture, "Spell_Nature_FaerieFire") then ff = true end
+		if string.find(texture, "Spell_Shadow_BlackPlague") then sw = true end
+		if string.find(texture, "Ability_Warrior_Sunder") then _, sa = true, applications end
+		i = i + 1
 	end
+	if cos and coe and cor and ff and sw and sa then return end
+
+	local out = "Target is missing "
+	if not cos then out = out.."CoS! " end
+	if not coe then out = out.." CoE! " end
+	if not cor then out = out.."CoR! " end
+	if not ff then out = out.."FF! " end
+	if not sw then out = out.."Shadow Weaving! " end
+	if sa < 5 then out = out.."Sunders! " end
+
+	BC.r(out, BC_PREP)
 end
 
 function ReportBuffsOnTarget()
