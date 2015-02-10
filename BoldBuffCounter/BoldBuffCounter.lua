4602b26bb2bc64c70c2c070c534dc336828bec66
boldilocks <kallerotmg@github.com>
2015-02-10 18:36:20 +0100

    updated some tocs, rebranded and changed overbuffed into BoldBuffCounter.

---
+++ BoldBuffCounter/BoldBuffCounter.lua
@@ -0,0 +1,134 @@
+BBC = {}
+BBCConfig = {}
+BBCConfig.sound = 1
+BBCConfig.scale = 2
+BBCConfig.locked = 0
+BBCConfig.active = 1
+BBCConfig.cap = 24
+BBC.prep = "[BoldBuffCounter] "
+
+function BBC.OnLoad()
+	this:RegisterEvent("VARIABLES_LOADED")
+	this:RegisterEvent("PLAYER_AURAS_CHANGED")
+	SlashCmdList["BBC"] = BBC.SlashHandler
+	SLASH_BBC1 = "/bbc"
+end
+
+function BBC.SlashHandler(arg1)
+	local _, _, command, args = string.find(arg1, "(%w+)%s?(.*)")
+	if(command) then
+		command = strlower(command)
+	else
+		command = ""
+	end
+	if(command == "sound") then
+		if(args == "on") then
+			BBCConfig.sound = 1
+			BC.m("Sound enabled.", BBC.prep)
+		elseif(args == "off") then
+			BBCConfig.sound = 0
+			BC.m("Sound disabled.", BBC.prep)
+		end
+	elseif(command == "lock") then
+		if BBCConfig.locked == 1 then
+			BBCConfig.locked = 0
+			BoldBuffCounterFrameAnchor:Show()
+			BoldBuffCounterFrame:EnableMouse("true")
+			BC.m("Position unlocked.", BBC.prep)
+		else
+			BBCConfig.locked = 1
+			BoldBuffCounterFrameAnchor:Hide()
+			BoldBuffCounterFrame:EnableMouse("false")
+			BC.m("Position locked.", BBC.prep)
+		end
+	elseif(command == "reset") then
+		BBCConfig.locked = 0
+		BoldBuffCounterFrame:SetScale(2)
+		BBCConfig.scale = 2
+		BoldBuffCounterFrame:ClearAllPoints()
+		BoldBuffCounterFrame:SetPoint("CENTER", "UIParent")
+		BC.m("Position reset.", BBC.prep)
+	elseif(command == "scale") then
+		if(tonumber(args)) then
+			local newscale = tonumber(args)
+			BBCConfig.locked = 0
+			BoldBuffCounterFrame:SetScale(newscale)
+			BBCConfig.scale = newscale
+			BoldBuffCounterFrame:ClearAllPoints()
+			BoldBuffCounterFrame:SetPoint("CENTER", "UIParent")
+			BC.m("Scale set to: "..newscale, BBC.prep)
+		end
+	elseif(command == "cap") then
+		if(tonumber(args)) then
+			local newcap = tonumber(args)
+			BBCConfig.cap = newcap
+			BC.m("Buff cap set to: "..newcap, BBC.prep)
+		end
+	elseif(command == "help") then
+		BC.m("Command list:", BBC.prep)
+		BC.c("/bbc on/off", BBC.prep)
+		BC.m("/bbc sound on/off", BBC.prep)
+		BC.c("/bbc scale 1-3", BBC.prep)
+		BC.m("/bbc reset", BBC.prep)
+		BC.c("/bbc lock", BBC.prep)
+	elseif(command == "on") then
+		BoldBuffCounterFrame:Show()
+		BBCConfig.active = 1
+		BoldBuffCounterFrame:ClearAllPoints()
+		BC.m("BoldBuffCounter enabled.", BBC.prep)
+	elseif(command == "off") then
+		BoldBuffCounterFrame:Hide()
+		BBCConfig.active = 0
+		BC.m("BoldBuffCounter disabled.", BBC.prep)
+	elseif(command == "") then
+		if(BoldBuffCounterFrame:IsVisible()) then
+			BoldBuffCounterFrame:Hide()
+			BC.m("BoldBuffCounter disabled.", BBC.prep)
+		else
+			BoldBuffCounterFrame:Show()
+			BC.m("BoldBuffCounter enabled.", BBC.prep)
+		end
+	else
+		BC.m("Type /bbc help for a command list.", BBC.prep)
+	end
+end
+
+function BBC.OnEvent(event)
+	if BBCConfig.active == 1 then
+		if(event == "VARIABLES_LOADED") then
+			BoldBuffCounterFrame:SetScale(BBCConfig.scale)
+			BoldBuffCounterFrame:Show()
+			if BBCConfig.locked == 1 then
+				BoldBuffCounterFrameAnchor:Hide()
+				BoldBuffCounterFrame:EnableMouse("false")
+			else
+				BoldBuffCounterFrameAnchor:Show()
+				BoldBuffCounterFrame:EnableMouse("true")
+			end
+		elseif(event == "PLAYER_AURAS_CHANGED" and BoldBuffCounterFrame:IsVisible()) then
+			local numBuffs = 0
+			local buffIndex, untilCancelled
+			local i = 0
+			while true do
+				buffIndex, untilCancelled = GetPlayerBuff(i, "HELPFUL")
+				if buffIndex < 0 then break end
+				i = i + 1
+			end
+
+			BoldBuffCounterFrameText:SetText(i)
+			
+			if i >= BBCConfig.cap then
+				if BBCConfig.sound == 1 then
+					PlaySoundFile("Interface\\AddOns\\BoldBuffCounter\\beep.mp3")
+				end
+				BoldBuffCounterFrameText:SetTextColor(1,0,0)
+			elseif i > (BBCConfig.cap-3) then
+				BoldBuffCounterFrameText:SetTextColor(1,1,0)
+			else
+				BoldBuffCounterFrameText:SetTextColor(0,1,0)
+			end
+			
+			BoldBuffCounterFrameText:Show()
+		end
+	end
+end
---
+++ BoldBuffCounter/README.md
@@ -0,0 +1,26 @@
+#BoldBuffCounter 0.1
+
+#Author
+Boldilocks etc.
+Warsong, Feenix
+
+#Credit
+Alason aka Freddy and Cootie, author/maintainer of C'thun Warner
+which was a good starting point when creating this addon.
+
+#Notes
+Provides information about the amount of buffs you have.
+Shows the amount of buffs in green text when above 20, yellow text above 28
+and red text as well as a BEEP sound when at cap.
+Here are screenshots of the four different states using lower values for 
+testing purposes: http://imgur.com/a/ZR1oH (top right)
+
+
+#Commands
+* /bbc on/off - Turn BoldBuffCounter on or off.
+* /bbc cap 1-32 - Set your current buff cap.
+* /bbc sound on/off - Turn the beeping sound on or off (default on).
+* /bbc scale 1-3 - Set the scale of the icon (default 2).
+* /bbc reset - Reset the default position and scale of the addon.
+* /bbc lock - Toggle locking the icon so that it is (not) moveable with a mouse.
+* /bbc help - Show the command list in the game, without these descriptions.
